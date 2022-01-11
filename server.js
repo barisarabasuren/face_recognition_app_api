3e3d0122ddcc38f3aba7bd381376df31c72f87a3
barisarabasuren <63669033+barisarabasuren@users.noreply.github.com>
2022-01-11 12:04:52 +0100

    fixing database

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const db = require('knex')({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        ssl: true
+        // ssl: {
+        //     rejectUnauthorized: false
+        // }
     }
 });
 
